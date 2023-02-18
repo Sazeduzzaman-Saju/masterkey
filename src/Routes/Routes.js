bb631008c970a4459c40106ec399f9a5079e4492
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-02-18 15:08:43 +0600

    Structure Ready

---
+++ src/Routes/Routes.js
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import About from "../Page/About/About";
+import ErrorPage from "../Page/ErrorPage/ErrorPage";
+import Home from "../Page/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            },
+        ]
+    }
+])
---
+++ src/layout/Main.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { PuffLoader } from 'react-spinners';
+import Footer from '../Shared/Footer/Footer';
+import Header from '../Shared/Header/Header';
+
+const Main = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000);
+    }, [])
+    return (
+        <div>
+            {
+                loading ?
+                    <div className=''>
+                        <div className='row'>
+                            <div className='flex justify-center items-center' style={{ height: '100vh' }}>
+                                <PuffLoader color={'#4568dc'} loading={loading} size={150} />
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <>
+                        <Header></Header>
+                        <Outlet></Outlet>
+                        <Footer></Footer>
+                    </>
+            }
+
+        </div>
+    );
+};
+
+export default Main;
